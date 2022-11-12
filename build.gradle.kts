f4f1eed36b70b417fdb8b02066baf59268ca811f
Tim Pavone <tim.pavone@t-online.de>
2022-11-12 21:27:30 +0100

    chore: add an OpenAPI generator to generate the server interfaces

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("org.springframework.boot") version "2.7.5"
     id("io.spring.dependency-management") version "1.0.15.RELEASE"
+    id("org.openapi.generator") version "6.2.1"
     `java`
 }
 
@@ -24,11 +25,33 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springdoc:springdoc-openapi-ui:1.6.12")
 
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
+openApiGenerate {
+    generatorName.set("spring")
+    inputSpec.set("$rootDir/specs/aws-ip-address-ranges-filter-v1.0.yaml")
+    outputDir.set("$buildDir/generated")
+    configFile.set("$rootDir/config/api-config.yaml")
+}
+
+sourceSets {
+    main {
+        java {
+            srcDir("$buildDir/generated/src/main/java")
+        }
+    }
+}
+
 tasks.named<Test>("test") {
     useJUnitPlatform()
+}
+
+tasks {
+    "compileJava" {
+        dependsOn("openApiGenerate")
+    }
 }
