f0ffb380a3cce177a37b09d12e211c960a7f44ba
Tim Pavone <tim.pavone@t-online.de>
2022-11-13 18:22:01 +0100

    test: add an application smoke test that uses no mocking at all

--- src/test/java/de/timpavone1990/aws_ip_adress_ranges_filter/AwsIpAddressRangesFilterApplicationTest.java
+++ src/test/java/de/timpavone1990/aws_ip_adress_ranges_filter/AwsIpAddressRangesFilterApplicationTest.java
@@ -1,8 +1,27 @@
 package de.timpavone1990.aws_ip_adress_ranges_filter;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.http.HttpStatus.OK;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 class AwsIpAddressRangesFilterApplicationTest {
 
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void testGetEUIpAddressRanges() {
+        final var responseEntity = restTemplate.getForEntity("/v1/aws/ip-address-ranges?region=EU", String.class);
+        assertThat(responseEntity.getStatusCode()).isEqualTo(OK);
+
+        final var body = responseEntity.getBody();
+        assertThat(body).isNotBlank();
+        assertThat(body.lines()).allMatch(line -> line.matches("^(EU \\d{1,3}\\.\\d{1,3}.\\d{1,3}.\\d{1,3}/\\d{1,2})$"));
+    }
 }
