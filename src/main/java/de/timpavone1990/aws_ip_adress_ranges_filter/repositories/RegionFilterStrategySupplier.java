9448fba33c3237ca68b82196c680e865ac33671b
Tim Pavone <tim.pavone@t-online.de>
2022-11-16 08:48:04 +0100

    refactor: simplify RegionFilterStrategySupplier

--- src/main/java/de/timpavone1990/aws_ip_adress_ranges_filter/repositories/RegionFilterStrategySupplier.java
+++ src/main/java/de/timpavone1990/aws_ip_adress_ranges_filter/repositories/RegionFilterStrategySupplier.java
@@ -3,6 +3,7 @@ package de.timpavone1990.aws_ip_adress_ranges_filter.repositories;
 import de.timpavone1990.aws_ip_adress_ranges_filter.generated.model.RegionFilter;
 import org.springframework.stereotype.Component;
 
+import static de.timpavone1990.aws_ip_adress_ranges_filter.generated.model.RegionFilter.ALL;
 import static de.timpavone1990.aws_ip_adress_ranges_filter.repositories.RegionFilterStrategy.MATCH_ALL_STRATEGY;
 import static de.timpavone1990.aws_ip_adress_ranges_filter.repositories.RegionFilterStrategy.REGION_MATCH_STRATEGY;
 
@@ -10,12 +11,6 @@ import static de.timpavone1990.aws_ip_adress_ranges_filter.repositories.RegionFi
 public class RegionFilterStrategySupplier {
 
     public RegionFilterStrategy supplyRegionFilterStrategy(final RegionFilter region) {
-        if (region == null) {
-            return MATCH_ALL_STRATEGY;
-        }
-        return switch (region) {
-            case ALL -> MATCH_ALL_STRATEGY;
-            default -> REGION_MATCH_STRATEGY;
-        };
+        return region == null || region == ALL ? MATCH_ALL_STRATEGY : REGION_MATCH_STRATEGY;
     }
 }
