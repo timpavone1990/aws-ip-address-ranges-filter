fa20110bcf2f7d5553b258bc3700b959b00be9dc
Tim Pavone <tim.pavone@t-online.de>
2022-11-20 22:15:34 +0100

    test: adjust performance test thresholds

--- scripts/find-ranges-perf.js
+++ scripts/find-ranges-perf.js
@@ -4,8 +4,8 @@ export const options = {
     vus: 20,
     duration: "30s",
     thresholds: {
-        "http_req_duration": ["p(95)<5000"],
-        "http_req_failed": ["rate==0"]
+        "http_req_duration": ["p(95)<1000"],
+        "http_req_failed": ["rate<0.01"]
     },
 };
 
